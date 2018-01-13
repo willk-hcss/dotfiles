2129a9d6f744ea0b381a473af22addc5f8936361
Will Killian <github.user@forkmantis.com>
2018-01-13 06:21:24 -0600

    Get rid of Vundle

--- vim/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# This script will install vim goodies.
-
-source $DOTHOME/lib/common
-
-if [ ! -d "~/.vim/bundle/Vundle.vim" ]; then
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-fi
